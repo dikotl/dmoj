58d4e6b1a7a6f7080d81131cc2b5734626c9f5c5
dikotl <188217911+dikotl@users.noreply.github.com>
2025-10-21 21:13:05 +0300

    ADS002

---
+++ ads002-sort-edge-by-length/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+static class Program
+{
+    struct Edge
+    {
+        public int u, v, idx;
+    }
+
+    static void Main()
+    {
+        var lenToEdge = ReadInput();
+
+        foreach (var pair in lenToEdge)
+        {
+            var len = pair.Key;
+            var edge = pair.Value;
+
+            Console.WriteLine($"{edge.u} {edge.v} {len} {edge.idx}");
+        }
+    }
+
+    static SortedDictionary<int, Edge> ReadInput()
+    {
+        var numValues = Console.ReadLine().Split();
+        var M = int.Parse(numValues[1]);
+        var lenToEdge = new SortedDictionary<int, Edge>();
+
+        for (var i = 0; i < M; ++i)
+        {
+            var value = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            lenToEdge.Add(value[2], new Edge
+            {
+                u = value[0],
+                v = value[1],
+                idx = i + 1,
+            });
+        }
+
+        return lenToEdge;
+    }
+}
